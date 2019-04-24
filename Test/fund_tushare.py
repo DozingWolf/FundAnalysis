480f7e328b19b305550151903c89f28cb8dbe3f3
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2019-04-24 15:40:55 +0800

    add bussiness logical

--- Test/fund_tushare.py
+++ Test/fund_tushare.py
@@ -31,11 +31,13 @@ def getCCTVnews(ts_base,time):
 def getFundInformation(ts_base,market='E'):
     FundInformationList = ts_base.fund_basic(market=market,encoding='utf-8')
     return FundInformationList
-
+#
 def getFundPosition(ts_base,fundcode):
     FundPositionInfo = ts_base.fund_portfolio(ts_code=fundcode)
     return FundPositionInfo
 
+# 从接口获取基金数据后再获取
+# 因tushare接口存在超时的情况，这个函数仅可以在少量数据的情况下进行使用。若存在较大量数据，请使用DB获取方式
 def getMultiFundPosition(ts_base,fund_df):
     fundDataList = list(fund_df.itertuples(index=False))
     for key,value in enumerate(fundDataList):
@@ -51,6 +53,32 @@ def getMultiFundPosition(ts_base,fund_df):
                 time.sleep(1)
             print('Go! Next part of data!')
         FundPositionInfo_set.to_sql(name='FUND_POSITION_LIST', con=orcl12c, if_exists='append')
+        print(FundPositionInfo_set)
+    return FundPositionInfo_set
+
+# 从DB获取基金数据后再获取
+# 业务流程：
+#   1. 先获取基金列表，条件为未处理数据
+#   2. 取出基金列表后传参给函数
+#   3. 确认取到接口数据后进行回写基金表
+#   4. 考虑超时后自动重启数据获取
+#   5. 若没有获取到任何未处理的基金数据，退出
+def getMultiFundPositionFromDB(ts_base,fund_table):
+    fundDataList = fund_table
+    for key,value in enumerate(fundDataList):
+        print('No.',key)
+        FundPositionInfo_set = ts_base.fund_portfolio(ts_code=value)
+        if key>0 and key%59 == 0:
+            # time.sleep(60)
+            print('please wait a minutes...')
+            for i in range(0,60):
+                print(60-i)
+                if 60-i == 1:
+                    print('ready...')
+                time.sleep(1)
+            print('Go! Next part of data!')
+        FundPositionInfo_set.to_sql(name='FUND_POSITION_LIST', con=orcl12c, if_exists='append')
+        # update fund data
         print(FundPositionInfo_set)
     return FundPositionInfo_set
 
